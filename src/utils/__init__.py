acf76e989f1f1334a34a24e3067cca67e9ba7fa4
Sâmpetru D. Mario <119307188+mariosam23@users.noreply.github.com>
2026-05-11 18:32:17 +0300

    removed remaining code

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,4 @@
 from .logger import logger
 from .config import config
-from .model_manager import get_sentence_nlp
-from .citation_remover import remove_random_citations, CitationRemovalResult
 
-__all__ = ["logger", "config", "get_sentence_nlp", "remove_random_citations", "CitationRemovalResult"]
+__all__ = ["logger", "config"]
